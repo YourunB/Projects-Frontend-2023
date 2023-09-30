12abb52005b86dff2f03e16c7e0fa6f62fdfb344
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-09-30 21:06:45 +0300

    feat: add create iamges from api in main section

--- image-galery/index.js
+++ image-galery/index.js
@@ -11,3 +11,51 @@ const audioClick = document.getElementById('song-click');
 const btnPrev = document.getElementById('btn-prev');
 const btnNext = document.getElementById('btn-next');
 const btnUp = document.getElementById('btn-up');
+
+getImages(page, search);
+function getImages(page, search = 'all') {
+  let url = '';
+  if (search === 'all') url = `https://api.unsplash.com/photos?client_id=4-EJtgSsL_fig8yHRfZ9DaV7_DqqHQZoahL2MaYrEw0&page=${page}`;
+  else url = `https://api.unsplash.com/search/photos?client_id=4-EJtgSsL_fig8yHRfZ9DaV7_DqqHQZoahL2MaYrEw0&page=${page}&query=${search}`;
+  const options = {
+    method: 'GET',
+  };
+  fetchAsync();
+  async function fetchAsync() { 
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+
+      if (search === 'all') {
+        for (let i = 0; i < data.length; i++) {
+          createImage(data[i].urls.regular ,data[i].links.download, data[i].description, data[i].alt_description);
+        }
+      }
+
+      if (search !== 'all') {
+        for (let i = 0; i < data.results.length; i++) {
+          createImage(data.results[i].urls.regular ,data.results[i].links.download, data.results[i].description, data.results[i].alt_description);
+        }
+      }
+
+    } catch (error) {
+      if (error != "") {
+        console.log("Error: " + error + "\nCheck the app later.");
+      }
+    }
+  }
+}
+
+function createImage(smallImage, fullImage, description, altDescription) {
+  main.append(document.createElement('div'));
+  main.getElementsByTagName('div')[main.getElementsByTagName('div').length - 1].classList.add('cards');
+  main.getElementsByTagName('div')[main.getElementsByTagName('div').length - 1].append(document.createElement('img'));
+  main.getElementsByTagName('img')[main.getElementsByTagName('img').length - 1].classList.add('cards__image');
+  main.getElementsByClassName('cards__image')[main.getElementsByClassName('cards__image').length - 1].src = smallImage;
+  main.getElementsByClassName('cards__image')[main.getElementsByClassName('cards__image').length - 1].setAttribute('data-download', fullImage);
+  main.getElementsByClassName('cards__image')[main.getElementsByClassName('cards__image').length - 1].setAttribute('data-description', description);
+  main.getElementsByTagName('div')[main.getElementsByTagName('div').length - 1].append(document.createElement('p'));
+  main.getElementsByTagName('p')[main.getElementsByTagName('p').length - 1].classList = 'cards__description';
+  main.getElementsByClassName('cards__description')[main.getElementsByClassName('cards__description').length - 1].textContent = altDescription;
+}
+
