5470015452f573d532aeb0ac12aa8f4fb94821b4
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-09-30 21:16:41 +0300

    feat: add search images and clear search in gallery

--- image-galery/index.js
+++ image-galery/index.js
@@ -12,6 +12,65 @@ const btnPrev = document.getElementById('btn-prev');
 const btnNext = document.getElementById('btn-next');
 const btnUp = document.getElementById('btn-up');
 
+let page = 1;
+let search = 'all';
+
+inputSearch.addEventListener('keypress', () => {
+  if (event.key === 'Enter' && inputSearch.value.length > 0) {
+    clearCards();
+    page = 1;
+    search = inputSearch.value;
+    getImages(page, search);
+    page += 1;
+    setTimeout(() => { fillWindow(); }, 250);
+  }
+});
+
+btnSearch.addEventListener('click', () => {
+  if (inputSearch.value.length > 0) {
+    clearCards();
+    page = 1;
+    search = inputSearch.value;
+    getImages(page, search);
+    setTimeout(() => { fillWindow(); }, 250);
+  }
+});
+
+inputSearch.addEventListener('input', () => {
+  if (inputSearch.value === '' || inputSearch.value.length === 0) {
+    clearSearch.classList.remove('header__clear-btn-active');
+    btnSearch.classList.remove('header__search-btn-active');
+    clearCards();
+    page = 1;
+    search = 'all';
+    getImages(page, search);
+    setTimeout(() => { fillWindow(); }, 250);
+  } else {
+    clearSearch.classList.add('header__clear-btn-active');
+    btnSearch.classList.add('header__search-btn-active');
+  }
+});
+
+clearSearch.addEventListener('click', () => {
+  if (inputSearch.value.length > 0) {
+    clearSearch.classList.remove('header__clear-btn-active');
+    btnSearch.classList.remove('header__search-btn-active');
+    inputSearch.value = '';
+    clearCards();
+    page = 1;
+    search = 'all';
+    getImages(page, search);
+    setTimeout(() => { fillWindow(); }, 250);
+  }
+});
+
+function fillWindow() {
+  if (document.body.getBoundingClientRect().height <= window.screen.height) {
+    page += 1;
+    getImages(page, search);
+  }
+}
+
 getImages(page, search);
 function getImages(page, search = 'all') {
   let url = '';
