497bb9b6e53aec1cce8b09b1c19131cac5270308
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2024-07-15 17:05:44 +0300

    feat: add build project gallery for deploy

---
+++ image-gallery/index.js
@@ -0,0 +1,283 @@
+console.log("Оценка 70/60\n1. Вёрстка +10\n1.1. на странице есть несколько фото и строка поиска +5\n1.2. в футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс +5\n2. При загрузке приложения на странице отображаются полученные от API изображения +10\n3. Если в поле поиска ввести слово и отправить поисковый запрос, на странице отобразятся изображения соответствующей тематики, если такие данные предоставляет API +10\n4. Поиск +30\n4.1. при открытии приложения курсор находится в поле ввода +5\n4.2. есть placeholder +5\n4.3. автозаполнение поля ввода отключено (нет выпадающего списка с предыдущими запросами) +5\n4.4. поисковый запрос можно отправить нажатием клавиши Enter +5\n4.5. после отправки поискового запроса и отображения результатов поиска, поисковый запрос продолжает отображаться в поле ввода +5\n4.6. в поле ввода есть крестик при клике по которому поисковый запрос из поля ввода удаляется и отображается placeholder +5\n5. Очень высокое качество оформления приложения и/или дополнительный не предусмотренный в задании функционал, улучшающий качество приложения +10\n5.1. приложение имеет свой дизайн\n5.2. добавлена анимация для открытия/добавления изображений, описания изображений, взаимодействия с приложением\n5.3. сделан анимированный слайдер для просмотра изображений\n5.4. добавлена работа с сенсорным экраном, свайпы (перелистывания слайдов с картинками, закрытие свайпом вверх слайдера\n5.5. добавлена кнопка вверх при скролле изображений\n5.6. анимированный input и кнопки\n5.7. отображение описания картинки\n5.8. дополнительное(альтернативное) описание изображения в слайдере при наведении на 'i'\n5.8. закрытие слайдера в десктопной версии по клику \n5.9. дополнительная кнопка и слайдера для открытия исходного изображения в максимальном разрешении для сохранения в новом окне браузера");
+
+const inputSearch = document.getElementById('search-image');
+const clearSearch = document.getElementById('clear-search');
+const btnSearch = document.getElementById('btn-search');
+const main = document.getElementById('main');
+const imageFull = document.getElementById('full-image');
+const descriptionFull = document.getElementById('full-decription');
+const downloadLink = document.getElementById('download');
+const overlay = document.getElementById('overlay');
+const overlayBack = document.getElementById('overlay-back');
+const audioClick = document.getElementById('song-click');
+const btnPrev = document.getElementById('btn-prev');
+const btnNext = document.getElementById('btn-next');
+const btnUp = document.getElementById('btn-up');
+
+let page = 1;
+let search = 'all';
+
+inputSearch.addEventListener('keypress', () => {
+  if (event.key === 'Enter' && inputSearch.value.length > 0) {
+    clearCards();
+    page = 1;
+    search = inputSearch.value;
+    getImages(page, search);
+    page += 1;
+    setTimeout(() => { fillWindow(); }, 250);
+  }
+});
+
+btnSearch.addEventListener('click', () => {
+  if (inputSearch.value.length > 0) {
+    clearCards();
+    page = 1;
+    search = inputSearch.value;
+    getImages(page, search);
+    setTimeout(() => { fillWindow(); }, 250);
+  }
+});
+
+inputSearch.addEventListener('input', () => {
+  if (inputSearch.value === '' || inputSearch.value.length === 0) {
+    clearSearch.classList.remove('header__clear-btn-active');
+    btnSearch.classList.remove('header__search-btn-active');
+    clearCards();
+    page = 1;
+    search = 'all';
+    getImages(page, search);
+    setTimeout(() => { fillWindow(); }, 250);
+  } else {
+    clearSearch.classList.add('header__clear-btn-active');
+    btnSearch.classList.add('header__search-btn-active');
+  }
+});
+
+clearSearch.addEventListener('click', () => {
+  if (inputSearch.value.length > 0) {
+    clearSearch.classList.remove('header__clear-btn-active');
+    btnSearch.classList.remove('header__search-btn-active');
+    inputSearch.value = '';
+    clearCards();
+    page = 1;
+    search = 'all';
+    getImages(page, search);
+    setTimeout(() => { fillWindow(); }, 250);
+  }
+});
+
+function fillWindow() {
+  if (document.body.getBoundingClientRect().height <= window.screen.height) {
+    page += 1;
+    getImages(page, search);
+  }
+}
+
+function clearCards() {
+  let cards = document.getElementsByClassName('cards');
+  for (let i = cards.length - 1; i >= 0; i -= 1) {
+    cards[i].remove();
+  }
+}
+
+getImages(page, search);
+function getImages(page, search = 'all') {
+  let url = '';
+  if (search === 'all') url = `https://api.unsplash.com/photos?client_id=4-EJtgSsL_fig8yHRfZ9DaV7_DqqHQZoahL2MaYrEw0&page=${page}`;
+  else url = `https://api.unsplash.com/search/photos?client_id=4-EJtgSsL_fig8yHRfZ9DaV7_DqqHQZoahL2MaYrEw0&page=${page}&query=${search}`;
+  const options = {
+    method: 'GET',
+  };
+  fetchAsync();
+  async function fetchAsync() { 
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+
+      if (search === 'all') {
+        for (let i = 0; i < data.length; i++) {
+          createImage(data[i].urls.regular ,data[i].links.download, data[i].description, data[i].alt_description);
+        }
+      }
+
+      if (search !== 'all') {
+        for (let i = 0; i < data.results.length; i++) {
+          createImage(data.results[i].urls.regular ,data.results[i].links.download, data.results[i].description, data.results[i].alt_description);
+        }
+      }
+
+    } catch (error) {
+      if (error != "") {
+        console.log("Error: " + error + "\nCheck the app later.");
+      }
+    }
+  }
+}
+
+function createImage(smallImage, fullImage, description, altDescription) {
+  main.append(document.createElement('div'));
+  main.getElementsByTagName('div')[main.getElementsByTagName('div').length - 1].classList.add('cards');
+  main.getElementsByTagName('div')[main.getElementsByTagName('div').length - 1].append(document.createElement('img'));
+  main.getElementsByTagName('img')[main.getElementsByTagName('img').length - 1].classList.add('cards__image');
+  main.getElementsByClassName('cards__image')[main.getElementsByClassName('cards__image').length - 1].src = smallImage;
+  main.getElementsByClassName('cards__image')[main.getElementsByClassName('cards__image').length - 1].setAttribute('data-download', fullImage);
+  main.getElementsByClassName('cards__image')[main.getElementsByClassName('cards__image').length - 1].setAttribute('data-description', description);
+  main.getElementsByTagName('div')[main.getElementsByTagName('div').length - 1].append(document.createElement('p'));
+  main.getElementsByTagName('p')[main.getElementsByTagName('p').length - 1].classList = 'cards__description';
+  main.getElementsByClassName('cards__description')[main.getElementsByClassName('cards__description').length - 1].textContent = altDescription;
+}
+
+function throttle(callee, timeout) {
+  let timer = null
+
+  return function perform(...args) {
+    if (timer) return
+
+    timer = setTimeout(() => {
+      callee(...args)
+
+      clearTimeout(timer)
+      timer = null
+    }, timeout)
+  }
+}
+
+btnUp.addEventListener('click', () => {
+  window.scroll({top: 0, behavior: "smooth"});
+});
+
+window.addEventListener("scroll", throttle( () => {
+  const height = document.body.offsetHeight;
+  const screenHeight = window.innerHeight;
+  const scrolled = window.scrollY;
+  const threshold = height - screenHeight / 4;
+  const position = scrolled + screenHeight;
+
+  if (scrolled > 400) btnUp.classList.remove('unvisible');
+  else btnUp.classList.add('unvisible');
+  if (position >= threshold) {
+    page += 1;
+    setTimeout(() => { getImages(page, search); }, 250);
+  }
+}, 250));
+
+let images;
+let count = 0;
+
+document.body.addEventListener('click', () => {
+  audioClick.play();
+  if (event.target.classList == 'cards__image') {
+    document.body.classList.add('scroll-off');
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent) === false) document.body.classList.add('scroll-padding');
+    document.getElementsByTagName('footer')[0].classList.add('footer-padding');
+    imageFull.src = event.target.src;
+    (event.target.dataset.description !== 'null') ? descriptionFull.textContent = event.target.dataset.description : descriptionFull.textContent = 'There is no additional description to this image';
+    downloadLink.href = event.target.dataset.download;
+    overlay.classList.add('overlay__index_up');
+    overlayBack.classList.add('overlay__background_show');
+
+    images = document.getElementsByClassName('cards__image');
+    for (let i = 0; i < images.length; i++) if (images[i] === event.target) count = i;
+  } 
+  if (navigator.maxTouchPoints === 0) {
+    if (event.target.classList.value === 'overlay__background overlay__background_show' || event.target.classList.value === 'overlay__background_controls') {
+      overlayBack.classList.remove('overlay__background_show');
+      setTimeout(() => { 
+        overlay.classList.remove('overlay__index_up');
+        document.body.classList.remove('scroll-off');
+        document.body.classList.remove('scroll-padding');
+        document.getElementsByTagName('footer')[0].classList.remove('footer-padding');
+      },2000);
+    }
+  }
+})
+
+btnPrev.addEventListener('click', () => {
+  imageFull.classList.add('hide');
+  count -= 1;
+  if (count < 0) count = images.length - 1;
+  setTimeout(()=>{
+    imageFull.src = images[count].src;
+    downloadLink.href = images[count].dataset.download;
+    (images[count].dataset.description !== 'null') ? descriptionFull.textContent = images[count].dataset.description : descriptionFull.textContent = 'There is no additional description to this image';
+    imageFull.classList.remove('hide');
+    imageFull.classList.add('show');
+    setTimeout(() => {
+      imageFull.classList.remove('show');
+    }, 200);
+  },200);
+});
+
+btnNext.addEventListener('click', () => {
+  imageFull.classList.add('hide');
+  count += 1;
+  if (count > images.length - 1) count = 0;
+  setTimeout(()=>{
+    imageFull.src = images[count].src;
+    downloadLink.href = images[count].dataset.download;
+    (images[count].dataset.description !== 'null') ? descriptionFull.textContent = images[count].dataset.description : descriptionFull.textContent = 'There is no additional description to this image';
+    imageFull.classList.remove('hide');
+    imageFull.classList.add('show');
+    setTimeout(() => {
+      imageFull.classList.remove('show');
+    }, 200);
+  },200);
+});
+
+overlay.addEventListener('touchstart', function (event) {
+  touchstartX = event.changedTouches[0].screenX;
+  touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+overlay.addEventListener('touchend', function (event) {
+  touchendX = event.changedTouches[0].screenX;
+  touchendY = event.changedTouches[0].screenY;
+  handleGesture();
+}, false);
+
+
+function handleGesture() {
+  if (touchendX < touchstartX) { //Swiped Left
+    imageFull.classList.add('hide');
+    count += 1;
+    if (count > images.length - 1) count = 0;
+    setTimeout(()=>{
+      imageFull.src = images[count].src;
+      downloadLink.href = images[count].dataset.download;
+      (images[count].dataset.description !== 'null') ? descriptionFull.textContent = images[count].dataset.description : descriptionFull.textContent = 'There is no additional description to this image';
+      imageFull.classList.remove('hide');
+      imageFull.classList.add('show');
+      setTimeout(() => {
+        imageFull.classList.remove('show');
+      }, 200);
+    },200);
+  }
+
+  if (touchendX > touchstartX) { //Swiped Right
+    imageFull.classList.add('hide');
+    count -= 1;
+    if (count < 0) count = images.length - 1;
+    setTimeout(()=>{
+      imageFull.src = images[count].src;
+      downloadLink.href = images[count].dataset.download;
+      (images[count].dataset.description !== 'null') ? descriptionFull.textContent = images[count].dataset.description : descriptionFull.textContent = 'There is no additional description to this image';
+      imageFull.classList.remove('hide');
+      imageFull.classList.add('show');
+      setTimeout(() => {
+        imageFull.classList.remove('show');
+      }, 200);
+    },200);
+  }
+
+  if (touchendY < touchstartY) { // Swiped Up
+    overlayBack.classList.remove('overlay__background_show');
+    setTimeout(() => { 
+      overlay.classList.remove('overlay__index_up');
+      document.body.classList.remove('scroll-off');
+      document.body.classList.remove('scroll-padding');
+      document.getElementsByTagName('footer')[0].classList.remove('footer-padding');
+    },2000);
+  }
+}
+
+setTimeout(() => { fillWindow(); }, 250);
